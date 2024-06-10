4fc631ebbac2c0d5b891225f88d3a80b59aa99ea
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2024-06-11 02:48:13 +0800

    Added left and right as parameters

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 export class Node {
-    constructor(value = null) {
+    constructor(value = null, left = null, right = null) {
         this.value = value;
         this.left = null;
         this.right = null;
