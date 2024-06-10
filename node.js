ce5a7339aa912b5ccb5387fca16624d28b4d9948
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2024-06-11 02:57:44 +0800

    Added initial buildTree

--- node.js
+++ node.js
@@ -1,7 +1,7 @@
 export class Node {
     constructor(value = null, left = null, right = null) {
         this.value = value;
-        this.left = null;
-        this.right = null;
+        this.left = left;
+        this.right = right;
     }
 }
---
+++ tree.js
@@ -0,0 +1,36 @@
+import { Node } from "./node.js";
+
+class Tree {
+    constructor(arr) {
+        this.sortedArr = [...new Set(arr)].sort((a, b) => a - b)
+        this.root = this.buildTree(this.sortedArr)
+    }
+    
+    buildTree(arr) {
+        const midPoint = Math.floor(arr.length / 2)
+        const middle = arr[midPoint]
+        const left = arr.slice(0, midPoint);
+        const right = arr.slice(midPoint);
+
+        if (left.length <= 1 || right.length <= 1) return
+        if (arr.length === 0) return null
+        const root = new Node(middle, this.buildTree(left), this.buildTree(right))
+        return root
+    }
+
+    prettyPrint(node, prefix = "", isLeft = true) {
+        if (node === null) {
+          return;
+        }
+        if (node.right !== null) {
+          prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+          prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+    }
+}
+
+const newTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+console.log(newTree.root)
